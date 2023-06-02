b82a8c6a7dfb11585daefae50593ae8f52976f32
sparshgahlot1 <sparshgahlot0398@gmail.com>
2023-06-02 08:31:38 +0530

    Assest Loading and NavbarBuilding

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {Blog, Footer, Header, Features, Possibility, WhatGPT3} from './containers';
 import {CTA, Brand, Navbar} from './components';
+import './App.css';
 
 const App = () => {
   return (
