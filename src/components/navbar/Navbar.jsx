b5d7d17230611200635b0db883bdabdf0b6828ad
sparshgahlot1 <sparshgahlot0398@gmail.com>
2023-06-02 13:34:15 +0530

    setting navbar links

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -9,7 +9,14 @@ const Navbar = () => {
     <div className='gpt3__navbar'>
       <div className='gpt3__navbar-links'>
         <div className='gpt3__navbar-links_logo'>
-        <img src={logo} alt='logo' />
+          <img src={logo} alt='logo' />
+        </div>
+        <div className='gpt3__navbar-links_containers'>
+          <p><a href='#home'>Home</a></p>
+          <p><a href='#wgpt3'>What is GPT?</a></p>
+          <p><a href='#possibility'>Open AI</a></p>
+          <p><a href='#features'>Case Studies</a></p>
+          <p><a href='#blog'>Library</a></p>
         </div>
       </div>
     </div>
