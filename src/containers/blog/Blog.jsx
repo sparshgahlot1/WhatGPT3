52bfb9867452577a69d82f8bc09966871c9e102f
sparshgahlot1 <sparshgahlot0398@gmail.com>
2023-06-03 09:09:37 +0530

    footer.css is ready

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -6,7 +6,7 @@ import './blog.css';
 
 const Blog = () => {
   return (
-    <div className='gpt3__blog section__padding' id='#blog'>
+    <div className='gpt3__blog section__padding' id='blog'>
       <div className='gpt3__blog-heading'>
         <h1 className="gradient__text">A lot is happening, <br /> We are blogging about it.</h1>
       </div>
